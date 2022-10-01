477d10909832418ef1f78270066b19eee6af7b85
MahimaUddaraju <u.mahima@science.christuniversity.in>
2022-10-01 22:49:10 +0530

    Final

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-giy
+function calFun(){
+    var a=document.getElementById("n1").value;
+    var b=document.getElementById("n2").value;
+    var c=document.getElementById("n3").value;
+    console.log(Number(a)+Number(b));
+    d=Number(a)*Number(b)*Number(c)/100;
+    e=Number(a)+Number(d);
+    document.getElementById("result").innerHTML= "The Simple Interest is " +d;
+    document.getElementById("result2").innerHTML= "The Total Amount is " +e;
+  }
